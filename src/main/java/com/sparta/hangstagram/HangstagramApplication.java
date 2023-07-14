9d01c85b29809cbedfaf6fb0dda866b2f35678fe
ByHorizon <rptedpa5@naver.com>
2023-07-14 15:01:53 +0900

    init commit

---
+++ src/main/java/com/sparta/hangstagram/HangstagramApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hangstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangstagramApplication.class, args);
+	}
+
+}
